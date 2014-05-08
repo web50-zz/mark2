a4255ff37549cf9ed874f814d93ba63c1f5252aa
Fedot B Pozdnyakov 9* <9@u9.ru>
2014-05-08 21:25:24 +0400

    sys_set ошибка при анализе параметра на  silent исправлена

--- var/di/m2_item.di.php
+++ var/di/m2_item.di.php
@@ -95,7 +95,7 @@ class di_m2_item extends data_interface
 		$this->_flush();
 		$this->insert_on_empty = true;
 		$result = $this->extjs_set_json(false);
-		if($silent = true)
+		if($silent == true)
 		{
 			return $result;
 		}
