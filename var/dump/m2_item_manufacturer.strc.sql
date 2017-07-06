f06da9fd293a03af162fcca6ce8429a674af4944
Fedot B Pozdnyakov 9* <9@u9.ru>
2017-07-06 17:47:08 +0300

    m2_item_manufacturer структура откорректирована

--- var/dump/m2_item_manufacturer.strc.sql
+++ var/dump/m2_item_manufacturer.strc.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `m2_item_manufacturer` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `item_id` smallint(5) unsigned NOT NULL,
-  `order` smallint(5) unsigned NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) unsigned NOT NULL,
+  `order` int(11) unsigned NOT NULL,
   `manufacturer_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `ssgn_project_id` (`item_id`),
