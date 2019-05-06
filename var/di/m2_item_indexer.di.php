7f0a34ac1953a60eb4809950dd8b64e896e2f3f5
Fedot B Pozdnyakov 9* <9@u9.ru>
2019-05-06 23:35:25 +0300

    промежуточный коммит говна перед мержем

--- var/di/m2_item_indexer.di.php
+++ var/di/m2_item_indexer.di.php
@@ -22,7 +22,7 @@ class di_m2_item_indexer extends di_index_processor
 	* @var	string	$name	Имя таблицы
 	*/
 	public $name = 'm2_item_indexer';
-	
+
 	/**
 	* @var	array	$fields	Конфигурация таблицы
 	*/
@@ -182,6 +182,163 @@ class di_m2_item_indexer extends di_index_processor
 	public function __construct () {
 		// Call Base Constructor
 		parent::__construct(__CLASS__);
+	}
+
+	protected function sys_batch_reindex(){
+		try{
+			$this->batch_reindex();
+			response::send(array('success' => true,'msg'=>'Переиндексировано'),'json');
+		}catch(Exception $e)
+		{
+			response::send(array('success' => false,'msg'=>$e->getMessage()),'json');
+		}
+	}
+	public function batch_reindex(){
+		$time_start = microtime(true); 
+		$items = $this->prepare_data();
+		$flds = array_keys($this->settings['index_target']['fields_to_index']);
+		$out_vals = '';
+		$j = 0;
+		$this->connector->exec('truncate table m2_item_indexer');
+		foreach($this->keys_index['m2_item'] as $k=>$v)
+		{
+			$j++;
+			$tmp = array();
+			$tmp['id'] = '';
+			foreach($flds as $k1=>$v1)
+			{
+				$t = (array)$v;
+				if($v1 == 'id')
+				{
+					$tmp['item_id'] = $t[$v1];
+				}
+				else
+				{
+					$tmp[$v1] = str_replace('"','\"',str_replace("'","\'",$t[$v1]));
+				}
+			}
+			foreach($this->settings['composite_fields'] as $k2=>$v2)
+			{
+				$t2 = $this->keys_index[$v2['di_name']][$tmp['item_id']];
+				if(count($t2) > 0)
+				{
+					$tmp[$v2['index_field_name']] = $this->json_enc($t2);		
+				}
+				else
+				{
+					$tmp[$v2['index_field_name']] = '[]';		
+				}
+			}
+				$out_vals[] = '("'.implode('","',array_values($tmp)).'",now())';
+				if($k == 1)
+				{
+					$out_flds = $tmp;
+					$out_flds['last_changed'] = 1;
+				}
+			if($j  == 500) 
+			{
+				$j = 0;
+				$flds_a = '(`'.implode('`,`',array_keys($out_flds)).'`)';
+				$sql = 'insert into m2_item_indexer '.$flds_a.' values '.implode(",\r\n",$out_vals);
+				$this->connector->exec($sql);
+				$out_vals = array();
+			}
+
+		}
+		$flds_a = '(`'.implode('`,`',array_keys($out_flds)).'`)';
+		$sql = 'insert into m2_item_indexer '.$flds_a.' values '.implode(",\r\n",$out_vals);
+		$this->connector->exec($sql);
+		$time_end = microtime(true);
+		$sql = '';
+		$out_vals = '';
+		$this->keys_index = array();
+		$di = data_interface::get_instance('m2_url_indexer');
+		$di->reindex();
+//		dbg::write("Memory use after ALL : ".memory_get_peak_usage(true)/1024 ."K");
+		$execution_time = ($time_end - $time_start)/60;
+	}
+
+	public function prepare_data()
+	{
+		$flds = array_keys($this->settings['index_target']['fields_to_index']);
+		$main_table = $this->settings['index_target']['di_name'];
+		$sql = 'select `'.implode('`,`',$flds).'` from '.$main_table.';';
+		$res = $this->_get($sql)->get_results();
+		$this->keys_index = array();
+		$this->keys_index[$main_table] = $res;
+		foreach($this->settings['composite_fields'] as $k=>$v)
+		{
+			$joins = array();
+			$fields = array();
+			$fields1 = array();
+			$JOINS = '';
+			$ORDER  = '';
+			$fields1 = array_keys($v['fields']);
+			$fields1[] = $v['di_key'];
+			$this->keys_index[$v['di_name']] = array();
+			foreach($fields1 as $k4=>$v4)
+			{
+				$fields[] = 't.`'.$v4.'`';
+			}
+
+			$table = $v['di_name'];
+			if(array_key_exists('joins',$v))
+			{
+				foreach($v['joins'] as $k1=>$v1)
+				{
+					$joins[] = $k1 .' a on t.'.$v1['source_key'].' = a.`'.$v1['remote_key'].'`';
+					foreach($v1['fields'] as $k2=>$v2)
+					{
+						$fields[] = 'a.'.$v2;
+					}
+				}
+			}
+			if(count($joins) >0)
+			{
+				$JOINS = 'LEFT JOIN '.implode(',',$joins);
+			}
+			if(array_key_exists('order_field',$v))
+			{
+				$ORDER = ' order by `'.$v['order_field'] .'` '.$v['order_type'];
+			}
+			//dbg::write("Memory use before query : ".memory_get_peak_usage(true)/1024 ."K");
+			//dbg::write("$sql");
+
+			//Вариант 1 на версии php5.4 жрет меньше памяти чем вариант два на версии 5.6 как нистранно наоборот.
+			$sql = 'SELECT '.implode(",",$fields).' FROM '.$table.' t '.$JOINS.' '.$ORDER.';';
+			$c = $this->get_connector();
+			$c->fetchMethod = PDO::FETCH_ASSOC;
+			$c->exec($sql,array(),true,true);
+			$tmp = $this->get_results();
+			/*
+			//Вариант 2
+			$dbh = glob::get('dbh');
+			$dbh->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, FALSE);
+			$sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+			$sth->execute();
+			while($rows = $sth->fetch(PDO::FETCH_ASSOC)) 
+			{
+			    $tmp[] = $rows;
+			}
+			*/
+			foreach($tmp as $k5=>$k6)
+			{
+				$item_id = $k6[$v['di_key']];
+				if($item_id > 0)
+				{
+					if(!array_key_exists($item_id,$this->keys_index[$v['di_name']]))
+					{
+						$this->keys_index[$v['di_name']][$item_id] = array();
+					}
+					unset($k6->item_id);
+					unset($k6->m2_id);
+					$this->keys_index[$v['di_name']][$item_id][] = $k6;
+				}
+			}
+			//dbg::write("Memory use after query : ".memory_get_peak_usage(true)/1024 ."K");
+			$tmp = array();
+		}
+//		var_dump('data_mined');
 	}
 
 }
